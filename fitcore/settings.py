6c9605f1eb4d7c2ecc1ff55853d6cbcf56f3b58a
roniahamed <mdroniahamed56@gmail.com>
2025-05-02 21:57:13 +0600

    postgresql configure

--- fitcore/settings.py
+++ fitcore/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'fitcore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fitcore',
+        'USER':'postgres',
+        'PASSWORD':'roni',
+        'HOST': 'localhost',
+        'PORT':5432,
     }
 }
 
