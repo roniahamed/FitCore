1b6141d5d245264771f616f15ec529e250927e8f
roniahamed <mdroniahamed56@gmail.com>
2025-05-02 21:52:47 +0600

    installed all app

--- fitcore/settings.py
+++ fitcore/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin',
+    'auth',
+    'users'
+    'health',
+    'meal',
+    'workout',
+    'notification',
+    'payment',
 ]
 
 MIDDLEWARE = [
