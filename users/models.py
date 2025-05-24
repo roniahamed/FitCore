005c522988b2d5efd56ff852fd2b0e134a01ab9f
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-24 18:08:30 +0600

    Custom User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser 
 from django.db import models
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = None 
+    email = models.EmailField(unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
