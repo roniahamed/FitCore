51d374962c3c772c55bb8cdf16a9aaa65c7420e0
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-24 18:49:37 +0600

    update

--- fitcore/settings.py
+++ fitcore/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fitcore',
         'USER':'postgres',
-        'PASSWORD':'roni',
+        'PASSWORD':'admin',
         'HOST': 'localhost',
         'PORT':5432,
     }
