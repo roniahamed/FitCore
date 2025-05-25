8266caeae8252788f467a47b6cda6b33268a0db3
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-25 12:36:41 +0600

    build health models

--- health/models.py
+++ health/models.py
@@ -5,8 +5,8 @@ class HealthData(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='healthdata')
     data_type = models.CharField(max_length=100)
     value = models.FloatField()
-    recorded_at = models.DateTimeField(auto_naw_add=True)
-    harte_rate = models.IntegerField(null=True, Black=True)
+    recorded_at = models.DateTimeField(auto_now_add=True)
+    harte_rate = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.data_type} - {self.recorded_at}"
