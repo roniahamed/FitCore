6f879e4eb0037d87e20d7cf903e6c9d184def071
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-25 12:36:15 +0600

    build notification models

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+
+class Notifications(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notifications')
+    title = models.CharField(max_length=300)
+    message = models.TextField(blank=True, null=True)
+    type = models.CharField(max_length=50)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
