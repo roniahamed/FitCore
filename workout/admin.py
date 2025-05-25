8dd60ff2d0a3e58856b017de1c6646d24d59426d
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-25 16:38:04 +0600

    Workout and WorkoutView modles create

--- workout/admin.py
+++ workout/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Workout, WorkoutVideo
 
-# Register your models here.
+@admin.register(WorkoutVideo)
+class WorkoutVideoAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return request.user.is_staff
+    
+    def save_model(self,request, obj, form, change):
+        if not obj.uploaded_by:
+            obj.uploaded_by = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Workout)
--- workout/models.py
+++ workout/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Workout(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.FloatField()
+    difficulty = models.CharField(max_length=50)
+    category = models.CharField(max_length=100)
+    goals = models.CharField(max_length=100)
+    is_premium = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} - {self.duration}"
+
+
+class WorkoutVideo(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='photos', blank=True, null=True)
+    duration = models.IntegerField()
+    order_index = models.IntegerField(default=0)
+    is_free = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='uploaded_workout_videos')
+
+    def __str__(self):
+        return f"{self.title} ({self.workout.title})"
