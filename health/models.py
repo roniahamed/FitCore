9ae4b978e7271176753ef55851a4ef28ea84be50
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-25 11:58:43 +0600

    create Healthdata

--- health/models.py
+++ health/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class HealthData(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='healthdata')
+    data_type = models.CharField(max_length=100)
+    value = models.FloatField()
+    recorded_at = models.DateTimeField(auto_naw_add=True)
+    harte_rate = models.IntegerField(null=True, Black=True)
+
+    def __str__(self):
+        return f"{self.data_type} - {self.recorded_at}"
