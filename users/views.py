83d86bfba7973b14acccbf1e758aee5a2a23c695
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-26 18:18:39 +0600

    adding register, update and delete users views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from .models import CustomUser, UsersProfile
+from rest_framework import generics , permissions
+from .serializers import RegisterSerializer, UserUpdateSerializer
 
-# Create your views here.
+
+
+class UserRegister(generics.CreateAPIView):
+    authentication_classes = []
+    permission_classes = [permissions.AllowAny]
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+
+class UserUpdate(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserUpdateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+class UserDelete(generics.DestroyAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
