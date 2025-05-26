5cbabedc12837e3db2c3914c407b858a9812895d
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-26 16:05:44 +0600

    Create Register Serializers

---
+++ users/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True, label="Confirm Password")
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name','password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Your passwords didn't match."})
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create_user(**validated_data)
+        return user 
+    
