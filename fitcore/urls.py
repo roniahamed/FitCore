2b3285b04925e275664387c8e35bf33a854cc665
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-26 18:16:45 +0600

    adding user app in urls

--- fitcore/urls.py
+++ fitcore/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
+
+    # users App
+    path('api/auth/', include('users.urls')),
     
     
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
