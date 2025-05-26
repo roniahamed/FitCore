7053f4a1cb8ffaa790859d74f945285bed71ac63
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-26 13:08:29 +0600

    set jwt authentication

--- fitcore/settings.py
+++ fitcore/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Rest_Framework
     'rest_framework',
+    'rest_framework_simplejwt',
+
+
+    #Extra App
     'adminapp',
     'authapp',
     'users',
@@ -140,3 +146,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# REST_FRAMEWORK 
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
