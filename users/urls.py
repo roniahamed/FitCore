7b1d14f864e16593e349190bc574c448ebb3db47
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-27 22:30:44 +0600

    create jwt logout endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import UserRegister, UserDelete, UserUpdate
+from .views import UserRegister, UserDelete, UserUpdate, LogoutView
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
 
 urlpatterns = [
+        # Authentications
+
+    # jwt Authentication 
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
+
     path('register/', UserRegister.as_view(), name='user_register'),
     path('update/', UserUpdate.as_view(), name='user_update'),
     path('delete/', UserDelete.as_view(), name='user_delete'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from .models import CustomUser, UsersProfile
 from rest_framework import generics , permissions
 from .serializers import RegisterSerializer, UserUpdateSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 
@@ -17,9 +21,25 @@ class UserUpdate(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+
 class UserDelete(generics.DestroyAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"detail": "Successfully logged out."})
+        except Exception as e:
+            return Response({'error':'Invalid Token'})
+
