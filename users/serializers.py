e8cb627f02a9fd0438c26cfcceb105d194bd52ca
Roni Ahamed <mdroniahamed56@gmail.com>
2025-05-31 15:29:42 +0600

    bug fix on OAuth2

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,6 @@ from .models import CustomUser
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True, label="Confirm Password")
-
     class Meta:
         model = CustomUser
         fields = ['email', 'first_name', 'last_name','password', 'password2']
@@ -24,16 +23,10 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'first_name', 'last_name','password']
-        extra_kwargs = {
-            'password':{'write_only':True, 'required':False}
-        }
+        fields = ['first_name', 'last_name']
 
     def update(self, instance, validate_data):
-        password = validate_data.pop('password', None)
         for attr, value in validate_data.items():
             setattr(instance,attr,value)
-        if password:
-            instance.set_password(password)
         instance.save()
         return instance
