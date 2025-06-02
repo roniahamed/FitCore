a6e41523f6169465679f76a9b2256b5037f26d2e
Roni Ahamed <mdroniahamed56@gmail.com>
2025-06-02 17:48:30 +0600

    adding google/lgoin/ urls

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from dj_rest_auth.registration.views import VerifyEmailView
+from .views import GoogleLogin
 
 urlpatterns = [
     # Authentications
@@ -8,5 +9,9 @@ urlpatterns = [
     path('registration/',include('dj_rest_auth.registration.urls')),
     path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     path('password-reset-confirm/<uidb64>/<token>/', lambda request, uidb64, token:None, name='password_reset_confirm'),
+
+    # Social Authentications
+    path('social/', include('allauth.socialaccount.urls')),
+    path('google/login/', GoogleLogin.as_view(), name='google_login'),
 ]
 
