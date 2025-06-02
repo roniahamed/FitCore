ec59ec08f65943f88501e8513f67af6474301cb4
Roni Ahamed <mdroniahamed56@gmail.com>
2025-06-02 17:47:59 +0600

    adding google login views

--- users/views.py
+++ users/views.py
@@ -1,1 +1,9 @@
 from .models import CustomUser
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = 'http://localhost:8000/api/auth/social/google/callback/'
+    client_class = OAuth2Client
