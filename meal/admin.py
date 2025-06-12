9cbfde986bb8756c7f3c51c8a432f9ee1acad92b
roniahamed <mdroniahamed56@gmail.com>
2025-06-12 11:24:01 +0600

    food admin setup

--- meal/admin.py
+++ meal/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Meal, MealPlan
+from .models import Meal, MealPlan, Food, MealItem, ScheduledMeal
 
 
-admin.site.register(Meal)
-admin.site.register(MealPlan)
+class MealItemInline(admin.TabularInline):
+    model = MealItem
+    extra = 1 # Default number of empty from to display 
+    autocomplete_fields = ['food'] # For easier searching if you have many food items
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'serving_quantity', 'serving_unit', 'calories', 'protein', 'carbohydrates', 'fat', 'food_category', 'user_added', 'is_public')
+    list_filter = ('food_category', 'is_public', 'user_added')
+    search_fields = ('name', )
+    fieldsets = (
+        (None ,{
+            'fields':('name', 'description', 'food_category', )
+        }),
+        ( 'Serving & Nutrition', {
+            'fields': (('serving_quantity', 'serving_unit'), ('calories', 'protein'), ('carbohydrates', 'fat'), ('fiber', 'sugar', 'sodium'))
+        }),
+        ('User & Visibility', {
+            'fields' : ('user_added', 'is_public')
+        })
+    )
+
