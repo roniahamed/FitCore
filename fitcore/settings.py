23a451392d6f08b0d0ac052fa40af96236eca681
roniahamed <mdroniahamed56@gmail.com>
2025-06-17 17:38:06 +0600

    Fitcore setting update

--- fitcore/settings.py
+++ fitcore/settings.py
@@ -228,7 +228,8 @@ REST_FRAMEWORK = {
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':10
+    'PAGE_SIZE':10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 
