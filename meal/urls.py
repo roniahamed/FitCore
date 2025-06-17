ef957a458c9e59f262384dc5b7a06744060c3eed
roniahamed <mdroniahamed56@gmail.com>
2025-06-17 17:51:04 +0600

    fix bug

--- meal/urls.py
+++ meal/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MealViewSet, FoodViewSet, MealPlanViewSet
 
-router = DefaultRouter
-router.register(r'food', FoodViewSet, basename='food' )
-router.register(r'meals', MealPlanViewSet, basename='meal')
+router = DefaultRouter()
+router.register(r'foods', FoodViewSet, basename='food' )
+router.register(r'meals', MealViewSet, basename='meal')
 router.register(r'meal-plans', MealPlanViewSet, basename='mealplan' )
 
 
