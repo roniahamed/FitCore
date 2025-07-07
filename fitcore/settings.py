10cc41c50cbeaa6ff3880b8a073ba523428658b7
roniahamed <mdroniahamed56@gmail.com>
2025-07-07 12:25:05 +0600

    staticfiles deleted

--- fitcore/settings.py
+++ fitcore/settings.py
@@ -98,9 +98,6 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'fitcore.urls'
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 TEMPLATES = [
     {
@@ -170,8 +167,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Media File
